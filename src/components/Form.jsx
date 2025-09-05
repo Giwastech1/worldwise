3d17526f916e5ece77ea5d0528e3b828ed4e579c
Giwa Toheeb <giwatoheeb27@gmail.com>
2025-09-06 00:22:27 +0100

    change back Button to a component and add back button to city

--- src/components/Form.jsx
+++ src/components/Form.jsx
@@ -4,6 +4,7 @@ import { useState } from "react";
 import { useNavigate } from "react-router-dom";
 import styles from "./Form.module.css";
 import Button from "./Button";
+import BackButton from "./BackButton";
 export function convertToEmoji(countryCode) {
   const codePoints = countryCode
     .toUpperCase()
@@ -50,10 +51,7 @@ function Form() {
 
       <div className={styles.buttons}>
         <Button type="primary">Add</Button>
-        <Button type="back" onClick={(e) => {
-          e.preventDefault();
-          navigation(-1);
-        }}>&larr; Back</Button>
+        <BackButton />
       </div>
     </form>
   );
