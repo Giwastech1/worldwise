11512aef07ae3432464fc456d07a15c09a8db761
Giwa Toheeb <giwatoheeb27@gmail.com>
2025-09-10 00:31:18 +0100

    remove duplicate city data from cities.json and dynamically setting marker indicator on the map

--- src/components/Map.jsx
+++ src/components/Map.jsx
@@ -2,24 +2,26 @@ import { useNavigate, useSearchParams } from "react-router-dom";
 import { MapContainer, TileLayer, Marker, Popup } from "react-leaflet";
 import styles from "./Map.module.css";
 import { useState } from "react";
+import { useCities } from "../context/CitiesContext";
 function Map() {
     const [mapPosition, setMapPosition] = useState([40,0]);
     const navigation = useNavigate();
     const [searchParam, setSearchParam] = useSearchParams();
     const lat = searchParam.get("lat");
     const lng = searchParam.get("lng");
+    const { cities } = useCities();
     return (
         <div className={styles.mapContainer}>
-            <MapContainer center={mapPosition} zoom={13} scrollWheelZoom={false} className={styles.map} >
+            <MapContainer center={mapPosition} zoom={13} scrollWheelZoom={true} className={styles.map} >
                 <TileLayer
                     attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
                     url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
                 />
-                <Marker position={mapPosition}>
+                {cities.map((city) => <Marker position={[city.position.lat, city.position.lng]}>
                     <Popup>
                         A pretty CSS3 popup. <br /> Easily customizable.
                     </Popup>
-                </Marker>
+                </Marker>)}
             </MapContainer>
         </div>
     );
